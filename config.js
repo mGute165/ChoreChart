dbebb63dbf57eb620ab8129b5db391893febf540
mGute165 <goot165@gmail.com>
2026-04-23 18:08:19 -0700

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-// Paste your Supabase project values here.
-// Use the ANON PUBLIC key, never the service_role key.
-window.SUPABASE_URL = "https://zpwgowwgwxdwtqnfndyt.supabase.co";
-window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpwd2dvd3dnd3hkd3RxbmZuZHl0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY5Njg2MzksImV4cCI6MjA5MjU0NDYzOX0.gMgBxfhi9Y7pFAv5za_tztGxzVEJfLcrgEiiWI8FYlE";
